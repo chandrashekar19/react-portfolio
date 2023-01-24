6120b3cb3fd6f711da2c655113611d3c7a85c112
KalalChandu <kalalshannu19@gmail.com>
2023-01-24 10:04:24 +0530

    About.js file

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,46 +1,47 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-class About extends Component {
-   render() {
+const  About = (props) => {
 
-   if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+   const { data } = props;
+
+   if(data){
+      var Name = data?.name;
+      var ProfilePic = "images/"+data?.image;
+      var Bio = data?.bio;
+      var Street = data?.address.street;
+      var City = data?.address.city;
+      var State = data?.address.state;
+      var Zip = data?.address.zip;
+      var Phone= data?.phone;
+      var Email = data?.email;
+      var ResumeDownload = data?.resumedownload;
    }
 
    return (
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={ProfilePic} alt="Chandrashekar" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
+            <p>{Bio}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
+						   <span>{Name}</span><br />
+						   <span>{Street}<br />
+						         {City} {State}, {Zip}
                      </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
+						   <span>{Phone}</span><br />
+                     <span>{Email}</span>
 					   </p>
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={ResumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
@@ -48,8 +49,7 @@ class About extends Component {
       </div>
 
    </section>
-    );
-  }
+   );
 }
 
 export default About;
